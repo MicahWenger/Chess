7fb831fc62297fb86de2143c0093b7f515b1995e
Micah Wenger <104636687+MicahWenger@users.noreply.github.com>
2023-02-07 17:14:42 -0500

    Cleaned Up UI

--- Chess.py
+++ Chess.py
@@ -20,13 +20,13 @@ import MW_AI
 #static lists
 ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
 COLOR_LIST = dict({
-   'LIGHT_GRAY': Color(0.8, 0.8, 0.8, 1, mode='rgba'),
-   'DARK_GRAY': Color(0.2, 0.2, 0.2, 1, mode='rgba'),
-   'GRAY': Color(0.5, 0.5, 0.5, 1, mode='rgba'),
-   'BLACK': Color(0, 0, 0, 1, mode='rgba'),
-   'WHITE': Color(1, 1, 1, 1, mode='rgba'),
-   'GREEN': Color(0, 1, 0, 1, mode='rgba'),
-   'RED': Color(1, 0, 0, 1, mode='rgba')
+   'LIGHT_GRAY':Color(0.8, 0.8, 0.8, 1),
+   'DARK_GRAY': Color(0.2, 0.2, 0.2, 1),
+   'GRAY':      Color(0.5, 0.5, 0.5, 1),
+   'BLACK':     Color(  0,   0,   0, 1),
+   'WHITE':     Color(  1,   1,   1, 1),
+   'GREEN':     Color(  0,   1,   0, 1),
+   'RED':       Color(  1,   0,   0, 1)
 })
 # white
 PIECE_SETUP_W = dict({
@@ -71,7 +71,7 @@ class Player:
         if self.type != 'USER':
             return self.AI.getMove(board)
 
-PLAYERS = [Player('USER', 0), Player('RAND_AI', 1)]
+# PLAYERS = [Player('USER', 0), Player('RAND_AI', 1)]
 PLAYERS = [Player('USER', 0), Player('MW_AI', 1)]
 
 class Square:
@@ -80,10 +80,22 @@ class Square:
         self.pos = (loc[0] * length, loc[1]* length)
         self.size = (length, length)
         self.color = color
+        self.highlighted = self.selected = False
 
-    def draw(self, canvas, tempColor=None):
-        canvas.add(COLOR_LIST[tempColor if tempColor else self.color])
-        canvas.add( Rectangle(pos=self.pos, size=self.size) )
+    def draw(self, canvas):
+        color = COLOR_LIST[self.color]
+        alpha = 1
+        if self.highlighted:
+            # color = COLOR_LIST['RED']
+            alpha = 0.2
+        elif self.selected:
+            # color = COLOR_LIST['GREEN']
+            alpha = 0.7
+        # canvas.add(COLOR_LIST[color])
+        # canvas.add( Rectangle(pos=self.pos, size=self.size) )
+        with canvas:
+            Color(color.r, color.g, color.b, alpha)
+            Rectangle(pos=self.pos, size=self.size)
 
     def __repr__(self):
         return "square at x=" + str(self.loc[0]) + ", y=" + str(self.loc[1]) + ", color: " + self.color
@@ -139,60 +151,69 @@ class GameScreen(Screen):
         self.draw()
 
     def draw(self):
-        # print("drawing")
+        # draw squares
         self.canvas.clear()
-        [[sq.draw(self.canvas) for sq in r] for r in self.arr]
-
+        with self.canvas:
+            color = COLOR_LIST['GRAY']
+            screenSize = (self.chessConfig.dimX, self.chessConfig.dimY)
+            Color(color.r, color.g, color.b)
+            Rectangle(pos=(0,0), size=screenSize)
+        [[sq.draw(self.canvas) for sq in row] for row in self.arr]
+        # draw pieces
         for r in range(len(self.pieces)):
             for c in range(len(self.pieces[r])):
                 if type(self.pieces[r][c]) in (Piece, King):
                     self.pieces[r][c].draw(self.canvas)
 
-    def highlightValidMoves(self, piece):
-        moves = piece.getValidMoves(self.pieces)
-        for m in moves:
-            self.arr[m[0]][m[1]].draw(self.canvas, 'RED')
+    def setHighlightedSquares(self, piece):
+        for m in piece.getValidMoves(self.pieces):
+            self.arr[m[0]][m[1]].highlighted = True
+
+    def unHighlightAllSquares(self):
+        for row in self.arr:
+            for square in row:
+                square.highlighted = False
 
     def on_touch_down(self, event):
         pos = event.pos
         coords = getLocFromCoords(int(self.chessConfig.dimX / self.chessConfig.rows), pos)
         # print("click! " + str(coords))
-
+        self.unHighlightAllSquares()
         if self.turn == 0:
             if self.selectedPiece:
+                self.arr[self.selectedPiece.x][self.selectedPiece.y].selected = False
                 # if coords in self.selectedPiece.getValidMoves(self.copyBoard()):
                 if coords in self.selectedPiece.getValidMoves(self.pieces):
                     self.movePiece(coords)
                     self.turn = 1 - self.turn
                     self.event()
                 else:
-                    # de-color old selected piece
-                    self.draw()
                     #color and select new peice
                     piece = self.pieces[coords[0]][coords[1]]
                     if type(piece) in (Piece, King) and piece.team == self.turn: # set selected piece
                         self.selectedPiece = piece
-                        self.arr[coords[0]][coords[1]].draw(self.canvas, 'GRAY')
-                        self.pieces[coords[0]][coords[1]].draw(self.canvas)
-                        self.highlightValidMoves(piece)
+                        self.arr[coords[0]][coords[1]].selected = True
+                        # highlight squares with a valid move
+                        self.setHighlightedSquares(piece)
                     else:
                         print("not a valid move")
                         #self.selectedPiece = 0
             else:
                 piece = self.pieces[coords[0]][coords[1]]
                 if type(piece) in (Piece, King) and piece.team == self.turn: # set selected piece
-                    print("selecting " + str(piece))
+                    # print("selecting " + str(piece))
                     self.selectedPiece = piece
-                    self.arr[coords[0]][coords[1]].draw(self.canvas, 'GRAY')
-                    self.pieces[coords[0]][coords[1]].draw(self.canvas)
-                    self.highlightValidMoves(piece)
-
+                    self.arr[coords[0]][coords[1]].selected = True
+                    # highlight squares with a valie move
+                    for move in piece.getValidMoves(self.pieces):
+                        self.arr[move[0]][move[1]].highlighted = True
+                    # self.setHighlightedSquares(piece)
+        self.draw()
     def createGrid(self):
         squareLength = int(self.chessConfig.dimX / self.chessConfig.rows)
-        self.arr = [
-        [ Square((r,c), squareLength, list(COLOR_LIST.keys())[(r+c+1) % 2])
-        for c in range(self.chessConfig.cols) ]
-        for r in range(self.chessConfig.rows) ]
+        self.arr = [ [ Square((r,c), squareLength, list(COLOR_LIST.keys())[(r+c+1) % 2])
+            for c in range(self.chessConfig.cols) ]
+            for r in range(self.chessConfig.rows) ]
 
     def createPieces(self):
         squareLength = int(self.chessConfig.dimX / self.chessConfig.rows)
@@ -258,19 +279,22 @@ class Piece:
         self.x = x
         self.y = y
         self.length = length
-        path = 'images/' + ('black' if self.team == 1 else 'white')+'/'+ self.type.lower()+'.png'
+        self.path = 'images/' + ('black' if self.team == 1 else 'white')+'/'+ self.type.lower()+'.png'
         loc = getCoordsFromLoc(self.length,(self.x,self.y))
-        self.rect = Rectangle(source=path, pos=loc, size=(self.length,self.length))
+        self.rect = Rectangle(source=self.path, pos=loc, size=(self.length,self.length))
+        self.movedTwoSpaces = False
 
     def move(self, newX, newY):
+        if self.type == "PAWN":
+            if newY in (0,7):
+                self.queen()
+            if abs(self.y - newY) == 2:
+                self.movedTwoSpaces = True
         self.x = newX
         self.y = newY
         loc = getCoordsFromLoc(self.length,(self.x,self.y))
         self.rect.pos = loc
 
-        #if (self.type == "PAWN" and self.team == 0 and self.y == 7) or (self.team == 1 and self.y == 0):
-        if self.type == "PAWN" and (self.y == 7 or self.y == 0):
-            self.queen()
 
     def queen(self): # change pawn to queen
         self.type = 'QUEEN'
@@ -279,8 +303,10 @@ class Piece:
         self.rect = Rectangle(source=path, pos=loc, size=(self.length,self.length))
 
     def draw(self, canvas):
-        canvas.add(COLOR_LIST['WHITE'])
-        canvas.add(self.rect)
+        c = COLOR_LIST['WHITE']
+        with canvas:
+            Color(c.r, c.g, c.b, c.a)
+            Rectangle(source=self.path, pos=self.rect.pos, size=(self.length,self.length))
 
     def getValidMoves(self, board, deep=True):
     # def getValidMoves(self, board, onlyValid=True, deep=True):
@@ -430,6 +456,25 @@ class Piece:
                     m += 1
         return moves
 
+    def canBeJumped(self, board):
+        enemyPieces = []
+        for row in board:
+            for piece in row:
+                if type(piece) in (Piece, King) and piece.team != self.team:
+                    enemyPieces.append(piece)
+
+        # print(str(len(enemyPieces))+" enemy pieces")
+        for p in enemyPieces:
+            if (self.x, self.y) in p.getValidMoves(board, deep=False):
+                # special case where pawn cant jump a piece directly in front of it
+                if p.type == "PAWN":
+                    if self.x != p.x:
+                        return True # print("check")
+                else:
+                    # print("check")
+                    return True
+        print(str(self.team)+" can not be jumped")
+        return False
     #returns new board where move has been made
     def simulateMove(self, move, board):
         cpy = self.duplicateBoard(board)
@@ -515,7 +560,7 @@ class King(Piece):
             for piece in row:
                 if type(piece) in (Piece, King) and piece.team != self.team:
                     enemyPieces.append(piece)
-                    
+
         # print(str(len(enemyPieces))+" enemy pieces")
         for p in enemyPieces:
             if (self.x, self.y) in p.getValidMoves(board, deep=False):
